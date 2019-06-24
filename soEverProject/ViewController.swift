60e22b37d6d2a64b5518b8e09910e7511247ebf4
phdafoe <phdafoe@gmail.com>
2019-06-24 09:14:46 +0200

    Initial Commit

---
+++ soEverProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soEverProject
+//
+//  Created by Andres Felipe Ocampo Eljaiesk on 24/06/2019.
+//  Copyright © 2019 icologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
